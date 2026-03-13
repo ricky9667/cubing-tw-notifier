d2a142cc39b6b29ba44730d5bf4335b9ae3adc19
Ricky Hu <ricky9667@gmail.com>
2026-03-13 16:48:51 +0800

    fix: 🐛 fix strict HTML parsing for Telegram

--- src/main/kotlin/io/github/ricky9667/cubing_tw_notifier/service/TelegramNotificationService.kt
+++ src/main/kotlin/io/github/ricky9667/cubing_tw_notifier/service/TelegramNotificationService.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
+import org.springframework.web.util.HtmlUtils
 import java.time.Duration
 
 @Service
@@ -20,6 +21,9 @@ class TelegramNotificationService(
         setReadTimeout(Duration.ofSeconds(5))
     }
 
+    private fun escapeTelegramHtml(value: Any?): String =
+        HtmlUtils.htmlEscape(value?.toString() ?: "")
+
     private val restClient = RestClient.builder()
         .baseUrl("https://api.telegram.org")
         .requestFactory(requestFactory)
@@ -29,10 +33,10 @@ class TelegramNotificationService(
         val text = """
             📢 <b>有新的比賽了! New Competition Announced!</b>
             
-            🏆 <b>比賽名稱 Name</b>: ${event.name}
-            📅 <b>比賽日期 Date</b>: ${event.eventDate}
+            🏆 <b>比賽名稱 Name</b>: ${escapeTelegramHtml(event.name)}
+            📅 <b>比賽日期 Date</b>: ${escapeTelegramHtml(event.eventDate)}
             
-            🔗 <a href="${event.url}">查看比賽資訊 View Event Details</a>
+            🔗 <a href="${escapeTelegramHtml(event.url)}">查看比賽資訊 View Event Details</a>
         """.trimIndent()
 
         sendMessage(text)
@@ -42,11 +46,11 @@ class TelegramNotificationService(
         val text = """
             🚨 <b>報名開始了! Registration is Open!</b>
             
-            🏆 <b>比賽名稱 Name</b>: ${event.name}            
+            🏆 <b>比賽名稱 Name</b>: ${escapeTelegramHtml(event.name)}            
             
             快點開始報名不然要來不及了!
             Hurry up and register before spots fill up!
-            🔗 <a href="${event.url}/registration">馬上報名 Register Now</a>
+            🔗 <a href="${escapeTelegramHtml(event.url)}/registration">馬上報名 Register Now</a>
         """.trimIndent()
 
         sendMessage(text)
